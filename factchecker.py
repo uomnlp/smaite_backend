e1c784827f8736bc13061c153c2afc012bac7bed
Aditya Agarwal <aditya@student.manchester.ac.uk>
2022-07-13 07:01:14 +0100

    Implement custom google search API to retrieve evidence

--- factchecker.py
+++ factchecker.py
@@ -1,5 +1,6 @@
 import lorem
 from transformers import pipeline
+import requests
 
 def CheckFact(claim, mode):
     # Process the claim
@@ -16,3 +17,15 @@ def CheckFact(claim, mode):
             explanations.append(element['generated_text'])
     return explanations
     
+
+def retrieveEvidence(claim):
+    key = "AIzaSyCrhF3JQ_rbEpv1j9rXhUjNypjlrBbmJ2Y"
+    cx = "641c8f210757ecadd"
+    url = 'https://www.googleapis.com/customsearch/v1?key=%s&q=%s&cx=%s' % (key, claim, cx)
+    response = requests.get(url)
+    response = response.json()
+    results = []
+    if(int(response['searchInformation']['totalResults']) > 0):
+        for item in response['items']:
+            results.append({'title':item['title'], 'url': item['link']})
+    return results
