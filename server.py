674982198dd50374325ba85ff319af761609dbfc
Aditya Agarwal <aditya@student.manchester.ac.uk>
2022-07-13 07:01:48 +0100

    Send evidence to the frontend on the /fact_check endpoint

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_from_directory, request
 from flask_cors import CORS, cross_origin
-from factchecker import CheckFact;
+from factchecker import CheckFact, retrieveEvidence;
 
 app = Flask(__name__, static_url_path='', static_folder='../smaite_frontend/build')
 CORS(app, support_credentials=True)
@@ -11,9 +11,9 @@ def explanation():
     claim = request.args.get('claim')
     mode = request.args.get('mode')
     if(mode == "summarize"):
-        return {"claim": claim, "explanations":CheckFact(claim, "summarize"), "evidence": ["Evidence 1", "Evidence 2"]}
+        return {"claim": claim, "explanations":CheckFact(claim, "summarize"), "evidence": retrieveEvidence(claim)}
     elif(mode == "generate"):
-        return {"claim": claim, "explanations":CheckFact(claim, "generate"), "evidence": ["Evidence 1", "Evidence 2"]}
+        return {"claim": claim, "explanations":CheckFact(claim, "generate"), "evidence": retrieveEvidence(claim)}
     else:
         return {"claim": claim, "explanations":"Invalid mode selected."}
 
