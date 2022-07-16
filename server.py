097ac28c97130fab77f98425acaee8447d65e330
Aditya Agarwal <aditya@student.manchester.ac.uk>
2022-07-16 08:47:21 +0100

    Configure host and port

--- server.py
+++ server.py
@@ -60,4 +60,5 @@ def homepage():
     return "<h1>Backend is up and running</h1>"
 
 if(__name__ == "__main__"):
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
