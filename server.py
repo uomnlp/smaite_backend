fd643bbc7697455fcdbb96cd6a9318d5a3998263
Aditya Agarwal <aditya@student.manchester.ac.uk>
2022-08-06 02:47:19 +0100

    Make code modular

--- server.py
+++ server.py
@@ -9,35 +9,31 @@ from dotenv import load_dotenv
 import os
 load_dotenv() 
 
-if(not exists(os.environ.get("MODEL_PATH"))):
-    model = requests.get(os.environ.get("MODEL_LINK"), stream=True)
-    total = int(model.headers.get('content-length', 0))
-    with open(os.environ.get("MODEL_PATH"), 'wb') as file, tqdm(
-        desc=os.environ.get("MODEL_PATH"),
+
+def download_file(path, link):
+    response = requests.get(link, stream=True)
+    total = int(response.headers.get('content-length', 0))
+    with open(path, 'wb') as file, tqdm(
+        desc=path,
         total=total,
         unit='iB',
         unit_scale=True,
         unit_divisor=1024,
     ) as bar:
-        for data in model.iter_content(chunk_size=1024):
+        for data in response.iter_content(chunk_size=1024):
             size = file.write(data)
             bar.update(size)
+
+if not os.path.exists(os.environ.get("MODEL_EXTRACTION_PATH")):
+    os.makedirs(os.environ.get("MODEL_EXTRACTION_PATH"))
+
+if(not exists(os.environ.get("MODEL_PATH"))):
+    download_file(os.environ.get("MODEL_PATH"), os.environ.get("MODEL_LINK"))
     with zipfile.ZipFile(os.environ.get("MODEL_PATH"), 'r') as zip_ref:
         zip_ref.extractall(os.environ.get("MODEL_EXTRACTION_PATH"))
 
 if(not exists(os.environ.get("CORPUS_PATH"))):
-    corpus = requests.get(os.environ.get("CORPUS_LINK"), stream=True)
-    total = int(corpus.headers.get('content-length', 0))
-    with open(os.environ.get("CORPUS_PATH"), 'wb') as file, tqdm(
-        desc=os.environ.get("CORPUS_PATH"),
-        total=total,
-        unit='iB',
-        unit_scale=True,
-        unit_divisor=1024,
-    ) as bar:
-        for data in corpus.iter_content(chunk_size=1024):
-            size = file.write(data)
-            bar.update(size)
+    download_file(os.environ.get("CORPUS_PATH"), os.environ.get("CORPUS_LINK"))
 
 initialiseES()
 
